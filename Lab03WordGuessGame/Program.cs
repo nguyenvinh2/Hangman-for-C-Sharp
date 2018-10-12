3df8682628ddbc7a6235c476ef357ea7489fc77b
nguye <nguyenv2@outlook.com>
2018-10-12 14:10:59 -0700

    comments added set up testing

--- Lab03WordGuessGame/Program.cs
+++ Lab03WordGuessGame/Program.cs
@@ -5,6 +5,10 @@ namespace Lab03WordGuessGame
 {
   public class Program
   {
+    /// <summary>
+    /// just some into and calls up the titlescreen
+    /// </summary>
+    /// <param name="args"></param>
     public static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
@@ -12,6 +16,11 @@ namespace Lab03WordGuessGame
       TitleScreen();
       Console.ReadKey();
     }
+    /// <summary>
+    /// Displays titlescreen selection options
+    /// checks user input for invalid selection
+    /// otherwise redirects user based on option selection
+    /// </summary>
     public static void TitleScreen()
     {
       string path = "words.txt";
@@ -50,7 +59,16 @@ namespace Lab03WordGuessGame
         TitleScreen();
       }
     }
-
+    /// <summary>
+    /// another selection screen for settings
+    /// gives options, checks for valid input
+    /// and redirects users
+    /// the finally block probably isn't needed
+    /// in this case
+    /// </summary>
+    /// <param name="path">tells where to create word.txt file
+    ///in this case in the place of execution.
+    /// </param>
     public static void SettingsScreen(string path)
     {
       Console.WriteLine();
@@ -93,6 +111,15 @@ namespace Lab03WordGuessGame
         SettingsScreen(path);
       }
     }
+    /// <summary>
+    /// throws catch for invalid user input
+    /// actually for both titlescreen and settings
+    /// based on parameter passed
+    /// </summary>
+    /// <param name="input">user input - very dynamic</param>
+    /// <param name="menu">checks from what menu the user 
+    /// is giving the input
+    /// </param>
     public static void CheckSelectionInput(string input, string menu)
     {
       if (menu == "main" && input != "1" && input != "2" && input != "3")
@@ -105,6 +132,13 @@ namespace Lab03WordGuessGame
       }
 
     }
+    /// <summary>
+    /// uses streamwriter to add in text to words.text file
+    /// everything will be brought down to lowercasing
+    /// does reject empty inputs and throws nullexception
+    /// also have generic exception to catch any error that is missed
+    /// </summary>
+    /// <param name="path">same variable as before</param>
     public static void AddWords(string path)
     {
       Console.WriteLine();
@@ -137,6 +171,14 @@ namespace Lab03WordGuessGame
         SettingsScreen(path);
       }
     }
+    /// <summary>
+    /// gets input from user about what word to delete from "path"
+    /// Uses streamwriter and stream reader 
+    /// doesn't really delete but creates a temp files and copies everything from the original file except for the word being delete.
+    /// then deletes the original file and renames the temp file to the original file
+    /// does input check similar to add
+    /// </summary>
+    /// <param name="path"></param>
     public static void DeleteWords(string path)
     {
       Console.WriteLine();
@@ -192,6 +234,11 @@ namespace Lab03WordGuessGame
         SettingsScreen(path);
       }
     }
+    /// <summary>
+    /// this one just reads the text file from "path" using StreamReader
+    /// no user input
+    /// </summary>
+    /// <param name="path"></param>
     public static void ViewWords(string path)
     {
       Console.WriteLine();
@@ -207,6 +254,11 @@ namespace Lab03WordGuessGame
       Console.ReadKey();
       SettingsScreen(path);
     }
+    /// <summary>
+    /// simple input check for error throw
+    /// no empty inputs
+    /// </summary>
+    /// <param name="answer">user input in string format</param>
     public static void CheckEmptyAnswer(string answer)
     {
       if (answer.Length < 1)
@@ -214,6 +266,15 @@ namespace Lab03WordGuessGame
         throw new ArgumentNullException();
       }
     }
+    /// <summary>
+    /// this is called from main menu
+    /// just provides the intro
+    /// selects a random word
+    /// and sends it to the method that actually handles
+    /// the game "gameOverallSequence"
+    /// </summary>
+    /// <param name="path">the location of the words.txt file that is being
+    /// chained passed through these methods</param>
     public static void PlayGame(string path)
     {
       Console.WriteLine();
@@ -223,6 +284,11 @@ namespace Lab03WordGuessGame
       string chosenWord = GetActualWord(wordLineCount, path);
       gameOverallSequence(path, chosenWord);
     }
+    /// <summary>
+    /// count how many words there are in the words.txt file so randomizer can be handled
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns>rnumber of words in text file</returns>
     public static int CountStoredLines(string path)
     {
       int countLines = 0;
@@ -242,7 +308,12 @@ namespace Lab03WordGuessGame
         throw;
       }
     }
-
+    /// <summary>
+    /// creates a random int within the range of the number of words in the text file. selects word based on that number
+    /// </summary>
+    /// <param name="wordLineCount">how many words avaiable to choose</param>
+    /// <param name="path"></param>
+    /// <returns>the chosen word for the game</returns>
     public static string GetActualWord(int wordLineCount, string path)
     {
       string chosenWord = "";
@@ -264,7 +335,24 @@ namespace Lab03WordGuessGame
         throw;
       }
     }
-
+    /// <summary>
+    /// handles the main game logic
+    /// breaks the chosen word to character array
+    /// creates new array with length to match chosen word array
+    /// renders the new array initial filed with "-" by calling up another method
+    /// accepts user input for letter guess
+    /// compares the input to the chosen word array
+    /// replaces the "-" with correct letter guess
+    /// checks logic if user won game 
+    /// repeats if user did not win game
+    /// also stores characters user inputs
+    /// creates new array of 50 characters to account for all keys on keyboard
+    /// adds it to the array after user inputs that specific key
+    /// if user wins, runs postgameoptions
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="chosenWord">the word the user has to guess
+    /// </param>
     public static void gameOverallSequence(string path, string chosenWord)
     {
       int guessCount = 0;
@@ -291,7 +379,12 @@ namespace Lab03WordGuessGame
       }
       PostGameOptions(path);
     }
-
+    /// <summary>
+    /// reveals the word onto the console as the user starts guessing the right letters
+    /// initially will be all "-"
+    /// </summary>
+    /// <param name="displayWordArray">what is being rendered onto console</param>
+    /// <param name="count">how many times the user guessed</param>
     public static void WordRendering(char[] displayWordArray, int count)
     {
       for (int i = 0; i < displayWordArray.Length; i++)
@@ -308,7 +401,12 @@ namespace Lab03WordGuessGame
       }
       Console.WriteLine();
     }
-
+    /// <summary>
+    /// checks if the array has any dashes left in it
+    /// if not, the user has fully guess the word
+    /// </summary>
+    /// <param name="wordArray">the arrray correctly guess letters</param>
+    /// <returns></returns>
     public static bool didWinGame(char[] wordArray)
     {
       int trackWord = 0;
@@ -331,17 +429,28 @@ namespace Lab03WordGuessGame
       }
       return false;
     }
-
+    /// <summary>
+    /// Without loops, this method is a little too convoluted
+    /// updates the array for already guessed letters 
+    /// so it can be rendered onto the console
+    /// </summary>
+    /// <param name="letterArray">an already of letters the user already guess</param>
+    /// <param name="letterGuess">the character the user guess</param>
     public static void LetterGuessRendering(char[] letterArray, char letterGuess)
     {
       int tracker = 0;
+      //checks if what the user inputted matches any of the already stored letters
       for (int i = 0; i < letterArray.Length; i++)
       {
         if (char.ToLowerInvariant(letterGuess) == letterArray[i])
         {
           tracker++;
+          Console.WriteLine();
+          Console.WriteLine("You already guessed this letter");
         }
       }
+      //if not, finds the next null space to store that letter in the array
+      //then breaks opertaion
       if (tracker == 0)
       {
         for (int i = 0; i < letterArray.Length; i++)
@@ -354,12 +463,19 @@ namespace Lab03WordGuessGame
         }
       }
       Console.WriteLine();
+      //renders out onto the console letters the user has already guess
       Console.Write("Letters guessed are: ");
       for (int i = 0; i < letterArray.Length; i++)
       {
         Console.Write($"{letterArray[i]} ");
       }
     }
+    /// <summary>
+    /// simply asks if the user wants to play again
+    /// and makes it so
+    /// if not, goes back to the titlescreen
+    /// </summary>
+    /// <param name="path"></param>
     public static void PostGameOptions(string path)
     {
       Console.WriteLine("Play Again? (Y/N)");
---
+++ XUnitLab03WordGuessGame/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using Lab03WordGuessGame;
+using static Lab03WordGuessGame.Program;
+
+namespace XUnitLab03WordGuessGame
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+
+    }
+  }
+}
